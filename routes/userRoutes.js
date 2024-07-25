2ea399d8458aee591c5020bcfea165749dc0ad7d
kirubasar <kiruba.sri.official@gmail.com>
2024-07-25 17:52:38 +0530

    login route added without jwt

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,4 +5,5 @@ const userController= require('../controller/userController')
 userRouter.get('/', userController.getAllUsers);
 userRouter.post('/', userController.register);
 userRouter.get('/:id', userController.getUserById);
-module.exports = userRouter
+userRouter.post('/login', userController.login)
+module.exports = userRouter;
