0aff3fa16347e96a69de8f941b7b1065d8d2580c
kirubasar <kiruba.sri.official@gmail.com>
2024-07-26 16:21:26 +0530

    updateUser & deleteUser endpoints done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,4 +12,7 @@ userRouter.post('/login', userController.login);
 userRouter.get('/', auth.verifyToken, userController.getAllUsers);
 userRouter.get('/logout',auth.verifyToken, userController.logout);
 userRouter.get('/:id',auth.verifyToken, userController.getUserById);
+userRouter.put('/:id', userController.updateUser);
+userRouter.delete('/:id', userController.deleteUser)
+
 module.exports = userRouter;
